12072804f38ffd0785cd6628f1c2953abda5f16a
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-07-13 17:39:37 +0100

    Add constants

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+export const fileExtensionSeparator = ".";
+export const fileFormat = fileExtensionSeparator + "png";
+export const pathSeparator = "/";
+
+export const skinsFolderPath = "skins";
+
+export const firstSkinFileName = "first-skin";
+export const secondSkinFileName = "second-skin";
+
+export const firstSkinFilePath = skinsFolderPath + pathSeparator + firstSkinFileName + fileFormat;
+export const secondSkinFilePath = skinsFolderPath + pathSeparator + secondSkinFileName + fileFormat;
